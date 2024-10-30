d425fa9310c8c3388e9def45b61749b96d9ff9ab
Nirdesh Kumar <nirdeshkumarom@gmail.com>
2024-10-31 01:32:55 +0530

    echo in php

---
+++ 01_echo.php
@@ -0,0 +1,18 @@
+<?php
+$name = "Nirdesh";
+$age = 21;
+$branch = "I.T";
+
+echo "Hello PHP<br>";
+
+echo $name;
+echo "<br>";
+echo $age;
+echo "<br>";
+echo $branch;
+echo "<br>"; 
+
+// echo function
+echo("As a Function");
+
+?>
