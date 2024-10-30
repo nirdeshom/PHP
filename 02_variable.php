5ff029ba02e12373922d6e21dd0cdcd148401e81
Nirdesh Kumar <nirdeshkumarom@gmail.com>
2024-10-31 01:35:46 +0530

    .\02_variable.php

---
+++ 02_variable.php
@@ -0,0 +1,14 @@
+<?php
+$name = "Nirdesh Verma";
+
+echo "$name <br />";
+echo "$name <br />";
+echo "$name <br />";
+echo "$name <br />";
+echo "$name <br />";
+
+$a = 5;
+$b = 10;
+$c = $a + $b;
+echo "$c";
+?>
